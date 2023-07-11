3da51e6a1cf4720dd2bf0a23a8471148190c1298
hung90909 <114125660+hung90909@users.noreply.github.com>
2023-07-11 16:40:17 +0700

    lab2

---
+++ controler/controlerTest.js
@@ -0,0 +1,19 @@
+const bodyParser = require('body-parser');
+const express = require('express')
+const multer = require("multer");
+const test = require("../model/test")
+const app = express();
+app.use(bodyParser.json())
+app.use(express.json())
+
+app.get('/getData' , async (req , res) =>{
+   try {
+      test.find()
+      .then(item => res.json(item))
+   } catch (error) {
+      console.log(error)
+   }
+})
+
+
+module.exports = app
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const methods = require('method-override');
+
+const test = require("./controler/controlerTest")
+const app = express();
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
+app.use(express.json())
+app.use(methods("_method"))
+const port = 9999;
+mongoose.connect('mongodb://127.0.0.1:27017/managerLab')
+    .then(function () {
+        console.log("ket noi thanh cong !")
+    })
+    .catch(function (err) {
+        console.log("error: " + err)
+    })
+
+app.use("/test", test)
+
+app.listen(port, function () {
+    console.log("running port " + port)
+}) 
