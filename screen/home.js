f50c2cd2c8b50d134ab21a91893d7071584b1f7b
hung90909 <114125660+hung90909@users.noreply.github.com>
2023-07-21 14:33:48 +0700

    update_lab4

--- screen/home.js
+++ screen/home.js
@@ -1,76 +1,95 @@
 import * as React from 'react';
-import { View, Text, TextInput, Button, Image } from 'react-native';
+import { View, Text, TextInput, Button, Image, FlatList } from 'react-native';
 import { useState, useEffect } from 'react';
 import { API_Test } from '../API/getAPI';
 export default function Home() {
     const [name, setName] = useState('')
-    const [mark , setMark] = useState('')
+    const [price, setPrice] = useState('')
+    const [dec, setDec] = useState('')
     const [data, setData] = useState([])
-   
-    const [textName , setTextName] = useState('')
-    const [textMark , setTextMark] = useState('')
-    const [count , setCount] = useState(0)
+
     const onData = () => {
         fetch(API_Test + '/getData')
             .then(item => item.json())
             .then(item => setData(item))
             .catch(err => console.log(err))
     }
-    const onSave = () =>{
-        fetch(API_Test + '/addData',{
-            method:"POST",
-            body: JSON.stringify({name , mark}),
-            headers:{
+    const onSave = () => {
+        fetch(API_Test + '/addData', {
+            method: "POST",
+            body: JSON.stringify({ name, price, dec }),
+            headers: {
                 "Content-Type": "application/json"
             }
         })
-        .catch((err) => console.log(err))
+            .catch((err) => console.log(err))
     }
-    
 
-    useEffect(() => {
-        onData()
-        getData()
-    }, [count])
-
-    const getData = async () => {
-        
-        data.map(item => {
-            setTextName(item.name)
-            setTextMark(item.mark)
-        })
-    }
     return (
         <View style={{
-         alignItems: "center",
-            flex: 1, paddingHorizontal:40
+            alignItems: "center",
+            flex: 1, paddingHorizontal: 40
         }}>
             <TextInput
-                onChangeText={(text) =>{
+                onChangeText={(text) => {
                     setName(text)
                 }}
                 style={{
-                    marginBottom: 40,marginTop:80, borderBottomWidth:1, height:40,
-                    width:"100%"
-                }} placeholder='Enter name' />
+                    marginBottom: 40, marginTop: 50, borderBottomWidth: 1, height: 40,
+                    width: "100%"
+                }} placeholder='Tên sản phẩm' />
             <TextInput
-              onChangeText={(text) =>{
-                setMark(text);
-              }}
-                style={{
-                    marginBottom: 40, borderBottomWidth:1, height:40,
-                    width:"100%"
-                }} placeholder='Enter mark' />
-            <Button
-                onPress={() => {
-                    onSave()
-                    setCount(count+1)
+                onChangeText={(text) => {
+                    setPrice(text);
                 }}
-                title='Send to API' />
-               {data && <Text style={{
-                    marginTop:30
-                }}>Name: {textName}</Text> }
-               {data && <Text>Mark: {textMark}</Text> }
+                style={{
+                    marginBottom: 40, borderBottomWidth: 1, height: 40,
+                    width: "100%"
+                }} placeholder='Giá sản phẩm' />
+            <TextInput
+                onChangeText={(text) => {
+                    setDec(text);
+                }}
+                style={{
+                    marginBottom: 40, borderBottomWidth: 1, height: 40,
+                    width: "100%"
+                }} placeholder='Mô tả' />
+            <View style={{
+                flexDirection: "row"
+            }}>
+                <Button
+                    onPress={() => {
+                        onSave()
+                    }}
+                    title='Insert' />
+                <View style={{
+                    width: 60
+                }} />
+                <Button
+                    onPress={() => {
+                        onData()
+
+                    }}
+                    title='Get' />
+            </View>
+            <FlatList
+            style={{
+                marginTop:30
+            }}
+                data={data}
+                keyExtractor={item => item._id}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={{
+                            flexDirection:"row", marginTop:10
+                        }}>
+                          <Text>Name:{item.name}     </Text>
+                          <Text>price:{item.price}   </Text>
+                          <Text>Desription:{item.dec}</Text>
+                        </View>
+                    )
+                }} />
+
         </View>
     )
 }
